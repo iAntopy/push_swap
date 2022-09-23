25f09031626f72fe5ef641ad4d801c827dc8434b
iAntopy <i-antony@hotmail.com>
2022-09-23 06:19:22 -0400

    push swap moves implemented but untested. Plus sweet stack print function that took way too much time to do. Was it worth it ? Oh yeaaaaaaaaaa

--- include/pswap.h
+++ include/pswap.h
@@ -6,7 +6,7 @@
 /*   By: iamongeo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 05:32:59 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/09/09 03:30:51 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/09/23 04:06:58 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define PSWAP_H
 
 # include <stdlib.h>
-# include <uni>
+# include <unistd.h>
 
 # include "libft.h"
 
@@ -31,7 +31,7 @@ typedef struct s_pswp
 	t_stk	stk_a;
 	t_stk	stk_b;
 
-}	t_pswp;
+}	t_psw;
 
 enum	e_err_codes
 {
---
+++ src/stack_moves.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_moves.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 03:11:01 by iamongeo          #+#    #+#             */
+/*   Updated: 2022/09/23 04:05:34 by iamongeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pswap.h"
+
+void	psw_rotate(t_stk *A, t_stk *B, int rev)
+{
+	int	temp;
+	int	len;
+	int	idx_from;
+	int	idx_to;
+
+	if (A && (A->len > 1))
+	{
+		len = A->len - 1;
+		idx_from = len * rev;
+		idx_to = len * !rev;
+		temp = A->arr[idx_from];
+		ft_memmove(A->arr + rev, A->arr + !rev, sizeof(int) * len);
+		A->arr[idx_to] = temp;
+	}
+	if (B && (B->len > 1))
+	{
+		len = B->len - 1;
+		idx_from = len * rev;
+		idx_to = len * !rev;
+		temp = B->arr[idx_from];
+		ft_memmove(B->arr + rev, B->arr + !rev, sizeof(int) * len);
+		B->arr[idx_to] = temp;
+	}
+}
+
+void	psw_swap(t_stk *A, t_stk *B)
+{
+	int	temp;
+
+	if (A && (A->len > 1))
+		ft_swap(A->arr, A->arr + 1);
+	if (B && (B->len > 1))
+		ft_swap(B->arr, B->arr + 1);
+}
+
+void	psw_push(t_stk *dst, t_stk *src)
+{
+	int	val;
+
+	if (src->len == 0)
+		return ;
+	val = src->arr[0];
+	if (src->len > 1)
+		ft_memmove(src->arr, src->arr + 1, sizeof(int) * (src->len - 1));
+	if (dst->len)
+		ft_memmove(dst->arr + 1, dst->arr, sizeof(int) * dst->len);
+	dst->arr[0] = val;
+	src->len--;
+	dst->len++;
+}
